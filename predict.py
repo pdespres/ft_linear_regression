a44081dadd1dc37ceecc1641c2fdaa10960ce9fc
Paul <33380838+pdespres@users.noreply.github.com>
2018-01-30 22:45:40 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,15 +8,25 @@ Return estimated price
 """
 
 def predict(mileage):
-	if mileage < 0:
-		print("Only positivie numbers")
+	if mileage(not number):
+		print("Please input a number")
 		exit(42)
-	
-	return price
+	if mileage < 0:
+		print("Only positive numbers")
+		exit(42)
+	if os.path.exists('theta.txt'):
+        	with open('theta.txt') as f:
+            		theta0 = float(f.readline())
+            		theta1 = float(f.readline())
+    	else:
+        	theta0 = 0
+        	theta1 = 0
+		
+	return (theta0 + mileage * theta1)
 
 if __name__ == "__main__":
 	argc = len(sys.argv)
 	if argc not in range(2, 3):
 		print(__doc__)
 	else:
-		print(predict(sys.argv[-1]))
+		print("Estimated car price: %g" % predict(sys.argv[-1]))
